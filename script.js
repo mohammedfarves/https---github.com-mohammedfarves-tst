3224d126d55a8547f7e09bce3117e8cf7ba4b4c4
MOHAMMED FARVES <142089394+mohammedfarves@users.noreply.github.com>
2024-11-21 23:30:16 +0530

    script.js

--- script.js
+++ script.js
@@ -112,3 +112,42 @@ function resetGame() {
 loadParagraph();
 inpField.addEventListener("input", initTyping);
 tryAgainBtn.addEventListener("click", resetGame);
+
+// Confirmation Modal
+const shareButton = document.getElementById("shareButton");
+const confirmationModal = document.getElementById("confirmationModal");
+const closeModal = document.getElementById("closeModal");
+const cancelButton = document.getElementById("cancelButton");
+
+shareButton.addEventListener("click", () => {
+  confirmationModal.classList.add("active");
+  confirmationModal.classList.add("open");
+  confirmationModal.style.display = "block";
+});
+
+closeModal.addEventListener("click", () => {
+  confirmationModal.classList.add("slideOut");
+  setTimeout(() => {
+    confirmationModal.classList.remove("active", "slideOut");
+    confirmationModal.style.display = "none";
+  }, 500); // Wait for the animation to complete (500ms)
+});
+
+cancelButton.addEventListener("click", () => {
+  confirmationModal.classList.add("slideOut");
+  setTimeout(() => {
+    confirmationModal.classList.remove("open", "slideOut");
+    confirmationModal.style.display = "none";
+  }, 500); // Wait for the animation to complete (300ms)
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === confirmationModal) {
+    confirmationModal.classList.remove("open");
+    setTimeout(() => {
+      confirmationModal.classList.remove("active");
+      confirmationModal.style.display = "none";
+    }, 500); // Wait for the animation to complete (500ms)
+  }
+});
+
